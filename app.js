e95440e76c01bf55f360fa825155d2d477cf5233
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-06 11:29:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let github = new GitHub();
 let ui = new UI();
+let githubrepo = new GitHubRepo();
 let userName = document.getElementById('userName');
 
 
@@ -17,11 +18,22 @@ function getUser(e) {
                 document.getElementById('profile').innerHTML = "Not Found!";
             }else{
                 ui.showProfile(data);
-                ui.showRepo(data);                
             }
         })
+
+        let z = githubrepo.getRepo(enterValue)
+        .then(data => {
+            if(data.message === "Not Found"){
+                document.getElementById('repo').innerHTML = "No Repositories!";
+            }else{
+                ui.showRepo(data);
+                console.log(data);
+            }
+        })
+
+
     }else{
-        console.log("Enter Value");
+        document.getElementById('message').innerHTML = "Enter Username!";
     }
 
 
